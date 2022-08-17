78c4db9c4fe210953f272253c1d0ff77d72b3a9c
Marymuriga <marymuriga@gmail.com>
2022-08-17 09:48:45 +0300

    commit tree fc820255e2507819a5288f0f44ff2b98ab5cca6e

---
+++ practice.rb
@@ -0,0 +1,14 @@
+dog = "cuddly"
+
+if dog == "hungry"
+  owner = "Refilling food bowl."
+elsif dog == "thirsty"
+  owner = "Refilling water bowl."
+elsif dog == "playful"
+  owner = "Playing tug-of-war."
+elsif dog == "cuddly"
+  owner = "Snuggling."
+else
+  owner = "Reading newspaper."
+end
+
